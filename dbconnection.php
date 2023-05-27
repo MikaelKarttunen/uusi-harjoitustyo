ddc40011216dff88e5ec72de58afe4384c44f43d
Mikael Karttunen <117356459+MikaelKarttunen@users.noreply.github.com>
2023-05-27 09:43:53 +0300

    Update dbconnection.php

--- dbconnection.php
+++ dbconnection.php
@@ -5,7 +5,8 @@ $password = "salasana";
 $dbname = "tietokannan_nimi";
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    $conn = new PDO("mysql:host=$host;dbname=$db", $username, $pw);
+        return $dbcon;
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch(PDOException $e) {
     echo "Virhe tietokantayhteydessä: " . $e->getMessage();
